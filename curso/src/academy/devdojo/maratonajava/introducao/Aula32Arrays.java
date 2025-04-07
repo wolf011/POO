999f8f2ede3610b72deae62cc123e0805e452c0e
wolf011 <duckhouse_bousoukis@aleeas.com>
2025-04-07 13:35:33 -0300

    organization

---
+++ curso/src/academy/devdojo/maratonajava/introducao/Aula32Arrays.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula32Arrays {
+	public static void main(String[] args) {
+		int[] idades = new int[3]; // tipo reference
+		idades[0] = 1;
+		idades[1] = 2;
+		idades[2] = 3;
+		System.out.println(idades[0]);
+
+		int[] numeros = { 1, 2, 3, 4, 5, 6 }; // Valor calculado de maneira automático
+		int[] nomerus = new int[] { 1, 2, 3, 4, 5, 6 };
+
+		// Percorre todos os espaços do array
+		for (int num : numeros) {
+			System.out.println();
+		}
+
+	}
+}
---
+++ curso/src/academy/devdojo/maratonajava/introducao/Aula36Arrays.java
@@ -0,0 +1,39 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula36Arrays {
+	public static void main(String[] args) {
+		int[][] dias = new int[2][2];
+		dias[0][0] = 31;
+		dias[0][1] = 28;
+
+		dias[1][0] = 31;
+		dias[1][1] = 28;
+
+//        for (int i = 0; i< dias.length; i++){
+//            for (int j = 0; j< dias[0].length; j++){
+//                System.out.println(dias[i][j]);
+//            }
+//
+//        }
+
+		for (int[] arrBase : dias) {
+			for (int num : arrBase) {
+				System.out.println(num);
+			}
+		}
+
+//        Concatenando arrays de diferentes tamanho
+		int[][] arrayInt = new int[3][];
+
+		arrayInt[0] = new int[2];
+		arrayInt[1] = new int[] { 1, 2, 3 };
+		arrayInt[2] = new int[] { 6, 5, 4, 3, 2, 1 };
+
+		for (int[] arrayBase : arrayInt) {
+			System.out.println("\n-----");
+			for (int num : arrayBase) {
+				System.out.print(num + " ");
+			}
+		}
+	}
+}
